8472ceb3f6deda8c22382ede176085b3b413c25d
Krishnadas Nair <kichoomalu@gmail.com>
2021-06-19 00:28:59 +0400

    Commit

--- app/src/main/java/com/example/spider_appdev_task_1/SpifactorCalculator.java
+++ app/src/main/java/com/example/spider_appdev_task_1/SpifactorCalculator.java
@@ -14,8 +14,7 @@ public class SpifactorCalculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spifactor_calculator);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        Objects.requireNonNull(getSupportActionBar()).hide();
+
+
     }
 }
